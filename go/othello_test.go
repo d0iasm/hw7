94223bc9c9a86519e6644fd77a3fb05247263275
Asami Doi <doiasami1219@gmail.com>
2017-07-14 15:42:24 +0900

    add minmax test

--- go/othello_test.go
+++ go/othello_test.go
@@ -1,9 +1,34 @@
 package othello
 
 import (
+	"reflect"
 	"testing"
+	"google.golang.org/appengine/aetest"
 )
 
 func TestMinMax(t *testing.T){
-
+	ctx, done, _ := aetest.NewContext()
+	defer done()
+	board := Board{
+		Pieces: [8][8]Piece{
+			{2, 2, 2, 2, 2, 2, 2, 2},
+			{2, 2, 2, 2, 2, 2, 2, 2},
+			{2, 2, 2, 2, 2, 2, 2, 2},
+			{1, 2, 1, 2, 2, 2, 2, 1},
+			{1, 2, 1, 2, 2, 2, 2, 1},
+			{1, 2, 1, 2, 2, 2, 2, 1},
+			{1, 2, 1, 2, 2, 2, 2, 1},
+			{1, 0, 0, 0, 0, 0, 0, 1},
+		},
+		Next: Black,
+	}
+	var move Move 
+	_, bestMove := board.Score(ctx, 3, move)
+	want := Move{
+		Where: Position{2, 8},
+		As: Black,
+	}
+	if !reflect.DeepEqual(bestMove, want) {
+		t.Errorf("bestMove = %v but want %v", bestMove, want)
+	}
 }
